789d52e11a66a9b036ee01edd8e7d47e7277d393
sampathoops <sampath.oops@gmail.com>
2017-09-21 18:01:41 +0530

    test

--- src/client/wizard/shareList.jsx
+++ src/client/wizard/shareList.jsx
@@ -9,20 +9,25 @@ import PropTypes from 'prop-types';
 
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
-let noteContent = '';
+let noteContent = [];
+let interval;
+//let currentContent = '';
 
-recognition.continuous = true;
+recognition.continuous = false;
 
 class ShareList extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      speaking: false
+      speaking: false,
+      start: false,
+      result: false
     }
 
     this.startRecognition = this.startRecognition.bind(this)
     this.stopRecognition = this.stopRecognition.bind(this)
+    this.captureVoice = this.captureVoice.bind(this)
   }
     propTypes: {
       match: PropTypes.object.isRequired,
@@ -31,47 +36,93 @@ class ShareList extends Component {
     }
     componentDidMount(){
       console.log('this.props.history:',this.props.history);
+
       var h = this.props.history;
       window.onCaptureComplete = function() {
         h.push('/share-list/update');
         $('#snap').hide();
       }
+      // window.self = this;
+      // var interval = setInterval(function() {
+      //     $('.circle_animation').css('stroke-dashoffset', initialOffset-((i+1)*(initialOffset/time)));
+      //     i++;
+      //     window.self.captureVoice();
+      // }, 2000);
     }
 
+    captureVoice() {
+      console.log('captureVoice');
+      var that = this;
+
+      //  debugger;
+      // $('.circle_animation').css('stroke-dashoffset', initialOffset-((i+1)*(initialOffset/time)));
+      // i++;
+        recognition.start();
+
+        recognition.onstart = function(event) {
+          $('.speak').show();
+          $('#micIcon').show();
+          $('.result').hide();
+        }
+
+        recognition.onresult = function(event) {
+          $('.speak').hide();
+          $('#micIcon').hide();
+          $('.result').show();
+          // this.setState({
+          //   result: true
+          // })
+        // debugger;
+        // event is a SpeechRecognitionEvent object.
+        // It holds all the lines we have captured so far.
+        // We only need the current one.
+        var current = event.resultIndex;
+
+        // Get a transcript of what was said.
+        var transcript = event.results[current][0].transcript;
+        // Add the current transcript to the contents of our Note.
+        // There is a weird bug on mobile, where everything is repeated twice.
+        // There is no official solution so far so we have to handle an edge case.
+        var mobileRepeatBug = (current == 1 && transcript == event.results[0][0].transcript);
+
+        if (!mobileRepeatBug) {
+          console.log(transcript)
+            noteContent.push(transcript);
+            fetch(`https://pixabay.com/api/?key=6502773-b63784a2647c85d2c0d01e277&q=${transcript}&image_type=photo&pretty=true`)
+            .then(response => response.json())
+            .then(result => {
+              $("#result").html(`<img class='searched-image' src=${result.hits[0].previewURL}></img>`);
+            })
+        }
+
+        // recognition.onend = function(event) {
+        //   this.setState({
+        //     result: false
+        //   })
+        // }
+        recognition.stop();
+    }
+  }
+
     startRecognition(e) {
+      console.log('startRecognition');
+
       this.setState({
         speaking: true
       })
-      if (noteContent.length) {
-       noteContent += ' ';
-      }
-      recognition.start();
-      setTimeout()
-    }
+
+      /* Need initial run as interval hasn't yet occured... */
+      //$('.circle_animation').css('stroke-dashoffset', initialOffset-(1*(initialOffset/time)));
+      this.captureVoice();
+      interval = setInterval(this.captureVoice, 3000);
+  }
 
     stopRecognition(e) {
-      this.setState({
-        speaking: false
-      })
+      $('.speak').hide();
+      $('#micIcon').show();
+      $('.result').hide();
       recognition.stop();
-      recognition.onresult = function(event) {
-
-      // event is a SpeechRecognitionEvent object.
-      // It holds all the lines we have captured so far.
-      // We only need the current one.
-      var current = event.resultIndex;
-
-      // Get a transcript of what was said.
-      var transcript = event.results[current][0].transcript;
-      // Add the current transcript to the contents of our Note.
-      // There is a weird bug on mobile, where everything is repeated twice.
-      // There is no official solution so far so we have to handle an edge case.
-      var mobileRepeatBug = (current == 1 && transcript == event.results[0][0].transcript);
-
-      if (!mobileRepeatBug) {
-          noteContent += transcript;
-      }
-      }
+      clearInterval(interval);
 
       $.ajax({
         url:"https://api.myjson.com/bins",
@@ -80,10 +131,10 @@ class ShareList extends Component {
         contentType:"application/json; charset=utf-8",
         dataType:"json",
         success: function(data, textStatus, jqXHR){
-          console.log(data, textStatus, jqXHR)
+          sessionStorage.setItem('shopping-list', JSON.stringify(data))
+          onCaptureComplete();
         }
       });
-      console.log(noteContent)
     }
 
     render(){
@@ -91,9 +142,17 @@ class ShareList extends Component {
         return (<div>
                 <div className="content">
                 <div id="selfieMsg">Once your shopping list is ready, tap the below green button.</div>
-                  <div><img id="micIcon" src="img/mic.png" style={{width:'85px',marginTop:'80px'}}></img></div>
-                  <div className="btn" style={{margin:'0 auto',zIndex:0,marginTop:'100px'}}>
-                <span onClick={(e) => this.state.speaking === false ? this.startRecognition(e) : this.stopRecognition(e)}>{this.state.speaking === false ? 'Read out Shopping list' : 'Save shopping list'}</span>
+                  <div>
+                    <img id="micIcon" src="img/mic.png" style={{width:'85px',marginTop:'80px'}}></img>
+                    <div className='speak blink'>Speak Now</div>
+                  </div>
+
+                <div id='result' className='result'>
+
+                </div>
+                  <div className="btn" style={{margin:'0 auto',zIndex:0,marginTop:'100px'}} onClick={(e) => this.state.speaking === false ? this.startRecognition(e) : this.stopRecognition(e) }>
+                <span>{this.state.speaking === false ? 'Start' : 'Save'}</span>
+
             </div>
                 </div>
               </div>
